1a2cc01815d1289c070fdcf2f376ce641d2c5e4d
Marco Seo <marco.dseo@gmail.com>
2020-01-06 18:51:08 -0500

    ready to deploy

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,7 @@ export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     const authLink = (
         <ul>
             <li><Link to="/profiles">Developers</Link></li>
+            <li><Link to="/posts">Posts</Link></li>
             <li><Link to="/dashboard">
                 <i className="fas fa-user"></i>{'  '}<span className="hide-sm">Dashboard</span></Link></li>
             <li><a onClick={logout} href="#!">
